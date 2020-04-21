f8536098e994aaeaea35498a006c5fcf14350892
Hamid Raza <hamidraza0123@gmail.com>
2020-04-21 18:50:35 +0530

    Add image in jumbotron and make it responsive

--- src/components/jumbotron.vue
+++ src/components/jumbotron.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="container">
-      <div class="jumbotron jumbotron-fluid bg-white">
-        <div class="container">
-            <h1 class="display-4">Hello, world!</h1>
-            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            <hr class="my-4">
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  <div class="jumbo container">
+        <div class="jumbotron jumbotron-fluid bg-white">
+            <div class="container">
+                <h1 class="display-4">Hello, world!</h1>
+                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr class="my-4">
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </div>
         </div>
-    </div>
+        <div class="img">
+            <img src="./cartoon-doctor.jpg" width="500px">
+        </div> 
   </div>
 </template>
 
@@ -19,5 +22,27 @@ export default {
 </script>
 
 <style>
+.jumbotron{
+    float: left;
+    width: 50%;
+    box-sizing: border-box;
+}
+.img{
+    float: right;
+    width: 50%;
+    box-sizing: border-box;
+}
+@media(max-width:1000px){
+    .jumbotron{
+        float: none;
+        width: 100%;
+        box-sizing: border-box;
+    }
+    .img{
+        float: none;
+        width: 100%;
+        box-sizing: border-box;
+    }
+}
 
 </style>
